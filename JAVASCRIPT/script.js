858468133a40db4a81ac8be8fc35462afcc0f308
Archita Mani <engineertripathi7@gmail.com>
2026-03-11 11:26:59 +0530

    inital commit

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,12 @@
+// This is a comment - JavaScript ignores this line
+        console.log("Hello from JavaScript!");
+        
+        // Show an alert popup
+        alert("Welcome to JavaScript!");
+        
+        // Change the page content
+        document.body.style.backgroundColor = "yellow";
+        const button = document.querySelector('button')
+        button.addEventListener('click', function() {
+    alert('Button clicked!');
+});
---
+++ JAVASCRIPT/var.js
@@ -0,0 +1,9 @@
+var name= "Archita tripathi";
+var age= 19;
+var isEmployed=  true;
+console.log(name);
+console.log(age);
+
+console.log(isEmployed);
+var age=19;
+console.log(age);
